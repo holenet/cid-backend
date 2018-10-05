9186b859fb93e97576ef0d7028d81b70da0c2f33
lameinthebox <lameinthebox@snu.ac.kr>
2018-10-06 00:53:58 +0900

    minor fix

--- chatbot/serializers.py
+++ chatbot/serializers.py
@@ -1,20 +1,2 @@
 from rest_framework import serializers
 from chatbot.models import Artist, SoloArtist, GroupArtist
-
-
-class ArtistSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Artist
-        fields = ('id', 'name', 'debut', 'agent')
-
-
-class SoloArtistSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SoloArtist
-        fields = ('id', 'name', 'debut', 'agent', 'birthday', 'member_of')
-
-
-class GroupArtistSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GroupArtist
-        fields = ('id', 'name', 'debut', 'agent', 'members')
