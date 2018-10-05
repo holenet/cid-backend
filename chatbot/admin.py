662311d0520023a0e9753116f23123636e6f7410
lameinthebox <lameinthebox@snu.ac.kr>
2018-10-05 22:35:25 +0900

    implemented user, album, music, evaluation, message

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Artist
+from .models import *
 
+admin.site.register(User)
 admin.site.register(Artist)
-
+admin.site.register(SoloArtist)
+admin.site.register(GroupArtist)
+admin.site.register(Album)
+admin.site.register(Music)
+admin.site.register(Evaluation)
+admin.site.register(Message)
--- chatbot/models.py
+++ chatbot/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User, UserManager
+from django.contrib.auth import models as auth_models
+
+
+class User(auth_models.User):
+    objects = auth_models.UserManager()
+
+    created = models.DateTimeField(auto_now_add=True)
+    gender = models.BooleanField(blank=True, default=0) # 0: male, 1: female
+    age = models.IntegerField(blank=True, default=0)
 
 
 class Artist(models.Model):
@@ -19,3 +27,45 @@ class SoloArtist(Artist):
 
 class GroupArtist(Artist):
     pass
+
+
+class Album(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    artist = models.ForeignKey('chatbot.Artist', related_name='albums', on_delete=models.CASCADE, blank=True, null=True)
+    release = models.DateField(blank=True, null=True)
+
+    class Meta:
+        ordering = ('created',)
+
+
+class Music(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    album = models.ForeignKey('chatbot.Album', related_name='music', on_delete=models.CASCADE, blank=True, null=True)
+    genre = models.CharField(max_length=100, blank=True, default='')
+    length = models.IntegerField(blank=True, default=0)
+
+    class Meta:
+        ordering = ('created',)
+
+
+class Evaluation(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('chatbot.User', related_name='evaluations', on_delete=models.CASCADE, blank=True, null=True)
+    music = models.ForeignKey('chatbot.Music', related_name='evaluations', on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        ordering = ('created',)
+
+
+class Message(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey('chatbot.User', related_name='sent', on_delete=models.CASCADE, blank=True, null=True)
+    receiver = models.ForeignKey('chatbot.User', related_name='received', on_delete=models.CASCADE, blank=True, null=True)
+    text = models.CharField(max_length=500, blank=True, default='')
+    music = models.ForeignKey('chatbot.Music', related_name='messages', on_delete=models.CASCADE, blank=True, null=True)
+    chips = models.CharField(max_length=100, blank=True, default='')
+
+    class Meat:
+        ordering = ('created',)
