4a70dee3c66ac0255f550d6b6a85ae4715ca0fc9
lameinthebox <lameinthebox@snu.ac.kr>
2018-10-26 05:21:14 +0900

    crawler collects title, artist and album

--- manager/crawler.py
+++ manager/crawler.py
@@ -1,7 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
 from django.db.models import signals
 from django.dispatch import receiver
 from manager.models import Crawler
 
-@receiver(signals.pre_save, sender=Crawler)
+
+@receiver(signals.post_save, sender=Crawler)
 def crawl(sender, instance, **kwargs):
-    print('crawler instance created!')
+    urls = {
+        'pop': 'https://www.melon.com/genre/song_list.htm?gnrCode=GN0900',
+        'rock': 'https://www.melon.com/genre/song_list.htm?gnrCode=GN1000',
+        'electronica': 'https://www.melon.com/genre/song_list.htm?gnrCode=GN1100',
+        'rap/hip-hop': 'https://www.melon.com/genre/song_list.htm?gnrCode=GN1200',
+        'R&B/soul': 'https://www.melon.com/genre/song_list.htm?gnrCode=GN1300',
+        'folk/blues/country': 'https://www.melon.com/genre/song_list.htm?gnrCode=GN1400',
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
+    }
+
+    html = requests.get(url=urls['pop'], headers=headers).text
+    soup = BeautifulSoup(html, 'html.parser')
+    song_list = soup.findChild(name='tbody')
+    rows = song_list.findChildren(name='tr')
+
+    for row in rows:
+        data = row.findChildren(name='td')
+
+        wrap = data[4].findChildren(name='a')
+        title = wrap[0].text
+        artist = wrap[1].text
+
+        warp = data[5].findChildren(name='a')
+        album = wrap[0].text
+
+        print(f'{album}: {title} - {artist}')
